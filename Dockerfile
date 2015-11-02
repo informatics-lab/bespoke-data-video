c6366c90a73f12062d23701e1eb72f5fb51abdab
Niall Robinson <niall.h.robinson@gmail.com>
2015-11-02 13:46:30 +0000

    initial import

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM quay.io/informaticslab/iris
+
+MAINTAINER niall.robinson@informaticslab.co.uk 
+
+RUN apt-get update \
+  && apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libav-tools libtheora-dev libtool libvdpau-dev libvorbis-dev pkg-config texi2html zlib1g-dev wget ffmpeg git \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN git clone https://github.com/met-office-lab/image-service.git
+
+RUN pip install --upgrade pip
+RUN cd ./image-service && pip install -r requirements.txt
+
+RUN wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz \
+  && tar xvzf yasm-1.2.0.tar.gz \
+  && cd yasm-1.2.0 \
+  && ./configure && make -j 4 && make install
+
+ENV PATH /opt/conda/bin:$PATH
+
+ADD [^.]* ./
+
+CMD ./videoservice.py
---
+++ procfile.py
@@ -0,0 +1,35 @@
+import sys
+import argparse as ap
+import subprocess as sp
+import os
+import glob
+
+import iris
+
+sys.path.append("./image-service/")
+import imageservice
+
+def proc_cube(cube, videoname, varname):
+    extent = [cube.coord("longitude").points.min(),
+              cube.coord("longitude").points.max(),
+              cube.coord("latitude").points.min(),
+              cube.coord("latitude").points.max(),]
+    for i, frt_cube in enumerate(cube.slices("forecast_reference_time")):
+        print "Processing timestep ", i, "...",
+        img_array, proced_data = imageservice.procDataToImage(frt_cube, "", ap.Namespace(extent=extent))
+        print "Writing image"
+        with open("data%03d.png" % i, "wb") as img:
+            imageproc.writePng(img_array, img, nchannels=3, alpha=False)
+    
+    print "Writing video"
+    sp.call(["avconv", "-y",
+            "-r", "1", "-i", "data%03d.png",
+            "-r", "1", "-vcodec", "libtheora", "-qscale:v", "2", videoname])
+    print "Cleaning up"
+    fs = glob.glob("./data???.png")
+    for f in fs:
+        os.remove(f)
+        
+if __name__="__main__":
+    cube = iris.load_cube(os.getenv("FILE_IN"))
+    proc_cube(cube, os.getenv("FILE_OUT"), os.getenv("VAR_NAME"))
